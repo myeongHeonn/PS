271edba040c60eadf22085cb59917640c45c4238
Myeongheon Oh <axm0219@naver.com>
2026-09-03 09:13:54 +0900

    [Passed] 각 정점까지의 최단 경로 (752ms, 40MB)

--- trail5/각 정점까지의 최단 경로/shortest-path-to-each-vertex.java
+++ trail5/각 정점까지의 최단 경로/shortest-path-to-each-vertex.java
@@ -1,77 +1,88 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
-class Node implements Comparable<Node> {
+class Edge implements Comparable<Edge>{
     int to;
     int cost;
     
-    public Node(int to, int cost) {
+    public Edge(int to, int cost) {
         this.to = to;
         this.cost = cost;
     }
 
     @Override
-    public int compareTo(Node o) {
+    public int compareTo(Edge o) {
         return this.cost - o.cost;
     }
 }
 
 public class Main {
     
-    public static int N;
-    public static int M;
-    public static int K;
-    public static ArrayList<Node>[] graph;
-    public static int[] dist;
+    static int N, M, K;
+    static ArrayList<Edge>[] graph;
+    static int[] dist;
     
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int K = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(br.readLine());
         
         graph = new ArrayList[N + 1];
-        dist = new int[N + 1];
+        for (int i = 1; i <= N; i++) graph[i] = new ArrayList<Edge>();
         
-        for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
-            dist[i] = 1_000_000_000;
-        }
+        dist = new int[N + 1];
+        for (int i = 1; i <= N; i++) dist[i] = 1_000_000_000;
         
         for (int i = 0; i < M; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            int w = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
             
-            graph[u].add(new Node(v, w));
-            graph[v].add(new Node(u, w));
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            
+            graph[a].add(new Edge(b, c));
+            graph[b].add(new Edge(a, c));
         }
-
+        
         dijkstra(K);
         
+        StringBuilder sb = new StringBuilder();
+        
         for (int i = 1; i <= N; i++) {
-            System.out.println((dist[i] == 1_000_000_000) ? -1 : dist[i]);
+            int value = (dist[i] == 1_000_000_000 ? -1 : dist[i]);
+            sb.append(value).append("\n");
         }
+        
+        System.out.println(sb);
     }
     
-    public static void dijkstra(int start) {
-        
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(start, 0));
+    static void dijkstra(int start) {
+        PriorityQueue<Edge> pq = new PriorityQueue<Edge>();
+        pq.add(new Edge(start, 0));
         dist[start] = 0;
         
         while (!pq.isEmpty()) {
-            Node now = pq.poll();
+            Edge now = pq.poll();
             
             if (now.cost > dist[now.to]) continue;
             
-            for (Node next : graph[now.to]) {
-                int newCost = dist[now.to] + next.cost;
+            for (Edge next : graph[now.to]) {
+                
+                int newCost = now.cost + next.cost;
+                
                 if (newCost < dist[next.to]) {
                     dist[next.to] = newCost;
-                    pq.offer(next);
+                    pq.add(new Edge(next.to, newCost));
                 }
             }
         }
-        
     }
 }
