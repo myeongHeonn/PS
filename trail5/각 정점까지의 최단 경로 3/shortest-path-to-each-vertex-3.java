8b108be8245e82b0f5c4791f39d18117c114e212
Myeongheon Oh <axm0219@naver.com>
2026-09-17 09:09:13 +0900

    [Passed] 각 정점까지의 최단 경로 3 (109ms, 9MB)

--- trail5/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.java
+++ trail5/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.java
@@ -1,48 +1,68 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+    
+    public static int INF = 1_000_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        int[][] graph = new int[n + 1][n + 1];
-        boolean[] visited = new boolean[n + 1];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         
-        for (int i = 0; i < m; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            int w = sc.nextInt();
+        int[][] graph = new int[N + 1][N + 1];
+        int[] dist = new int[N + 1];
+        boolean[] visited = new boolean[N + 1];
+        
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
             
-            graph[u][v] = w;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            
+            graph[a][b] = c;
         }
-        // Please write your code here.
-        int[] dist = new int[n + 1];
-        for (int i = 1; i <= n; i++) dist[i] = 1_000_000_000;
+        
+        for (int i = 1; i <= N; i++) {
+            dist[i] = INF;
+        }
+        
         dist[1] = 0;
         
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= N; i++) {
             int minIndex = -1;
             
-            for (int j = 1; j <= n; j++) {
+            for (int j = 1; j <= N; j++) {
                 if (visited[j]) continue;
                 
-                if (minIndex == -1 || dist[minIndex] > dist[j]) {
+                if (minIndex == -1 || dist[j] < dist[minIndex]) {
                     minIndex = j;
                 }
             }
             
             visited[minIndex] = true;
             
-            for (int j = 1; j <= n; j++) {
+            for (int j = 1; j <= N; j++) {
                 if (graph[minIndex][j] == 0) continue;
                 
-                dist[j] = Math.min(dist[j], dist[minIndex] + graph[minIndex][j]);
+                if (dist[j] > dist[minIndex] + graph[minIndex][j]) {
+                    dist[j] = dist[minIndex] + graph[minIndex][j];
+                }
             }
         }
         
-        for (int i = 2; i <= n; i++) {
-            System.out.println(dist[i] == 1_000_000_000 ? -1 : dist[i]);
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 2; i <= N; i++) {
+            int value = (dist[i] == INF) ? -1 : dist[i];
+            sb.append(value).append("\n");
         }
+        
+        System.out.println(sb);
     }
 }
